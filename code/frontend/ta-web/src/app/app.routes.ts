c4327ac7bb7970aefcc74201a4b4ba575132fe99
Jesus Acedo <jrodrigoav@gmail.com>
2024-03-08 13:50:30 -0600

    FrontEnd

---
+++ code/frontend/ta-web/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AboutComponent } from './pages/about/about.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent  },
+    { path: 'about', component: AboutComponent  },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
