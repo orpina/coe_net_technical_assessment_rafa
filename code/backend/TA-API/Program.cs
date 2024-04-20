9d4ea742b0d0426f8b1c39170c17e8f24a9bd418
Jesus Acedo <jrodrigoav@gmail.com>
2024-04-19 18:47:04 -0600

    Extras

--- code/backend/TA-API/Program.cs
+++ code/backend/TA-API/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using TA_API.Services.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 {
-
+    builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console());
+    builder.Services.AddDbContext<AssessmentDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("AssessmentDB")));
 }
 var app = builder.Build();
 {
+    app.UseSerilogRequestLogging();
     app.MapGet("/", () => "Technical Assessment API");
+    app.MapGet("/lbhealth", () => "Technical Assessment API");
 }
 app.Run();
---
+++ code/backend/TA-API/Services/Data/AssessmentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TA_API.Services.Data
+{
+    public class AssessmentDbContext : DbContext
+    {
+        public AssessmentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
